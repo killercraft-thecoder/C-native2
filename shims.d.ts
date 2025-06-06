87cc04ab00b8622dacd78b459215e6a2a78f7f30
killercraft-thecoder <Landonjcraft@outlook.com>
2025-06-05 17:12:37 -0700

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,17 +1,17 @@
 declare namespace Matrix {
     // Computes the sum of all elements in a matrix
     //% shim=pxt::Matrix::sum
-    function sum(matrix: any): number;
+    export function sum(matrix: any): number;
 
     // Adds two matrices element-wise
     //% shim=pxt::Matrix::add
-    function add(m1: any, m2: any): any;
+    export function add(m1: any, m2: any): any;
 
     // Subtracts two matrices element-wise
     //% shim=pxt::Matrix::sub
-    function sub(m1: any, m2: any): any;
+    export function sub(m1: any, m2: any): any;
 
     // Multiplies two matrices
     //% shim=pxt::Matrix::mul
-    function mul(m1: any, m2: any): any;
+    export function mul(m1: any, m2: any): any;
 }
