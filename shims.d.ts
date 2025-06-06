703bdded1f5b1bf01affb8389ba0f5788680ac68
killercraft-thecoder <Landonjcraft@outlook.com>
2025-06-05 17:05:46 -0700

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,18 +1,17 @@
-// Shims for native C++ functions
 declare namespace Matrix {
-    // Computes sum of all elements
-    //% shim=Matrix::sum
-    function sum(matrix: number[][]): number;
+    // Computes the sum of all elements in a matrix
+    //% shim=pxt::Matrix::sum
+    function sum(matrix: any): number;
 
     // Adds two matrices element-wise
-    //% shim=Matrix::add
-    function add(m1: number[][], m2: number[][]): number[][];
+    //% shim=pxt::Matrix::add
+    function add(m1: any, m2: any): any;
 
     // Subtracts two matrices element-wise
-    //% shim=Matrix::sub
-    function sub(m1: number[][], m2: number[][]): number[][];
+    //% shim=pxt::Matrix::sub
+    function sub(m1: any, m2: any): any;
 
     // Multiplies two matrices
-    //% shim=Matrix::mul
-    function mul(m1: number[][], m2: number[][]): number[][];
+    //% shim=pxt::Matrix::mul
+    function mul(m1: any, m2: any): any;
 }
